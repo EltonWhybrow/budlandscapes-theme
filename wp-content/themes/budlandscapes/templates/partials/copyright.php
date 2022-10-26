a127aa19cad4b351d25d7a3d1ed4e5c53f5c1500
Elton <eltonwhybrow@icloud.com>
2022-10-26 14:18:17 +0100

    chore: copyright copy

--- wp-content/themes/budlandscapes/templates/partials/copyright.php
+++ wp-content/themes/budlandscapes/templates/partials/copyright.php
@@ -1,4 +1,4 @@
 <div class="flex flex-wrap lg:container lg:mx-auto justify-center p-4">
     <p class="text-white pt-20 text-xs text-center">
-        All rights reserved &copy;<?php echo date("Y"); ?> Bud Design &amp; Landscaping Ltd | Site themed by <a class="primary-link text-xs" href="//widesign.co.uk" target="_blank">Wide Sign</a></p>
+        All rights reserved &copy;<?php echo date("Y"); ?> Bud Design &amp; Landscaping Ltd | Custom Theme <a class="primary-link text-xs" href="//widesign.co.uk" target="_blank">Wide Sign</a></p>
 </div>
